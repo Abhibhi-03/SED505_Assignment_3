82077e2c32bf4a2a1e312e33c99088dff273bcde
Abhi Patel <apatel477@myseneca.ca>
2023-10-27 23:06:59 -0400

    Added Projector.cs

---
+++ Projector.cs
@@ -0,0 +1,12 @@
+class Projector
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Projector is on.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Projector is off.");
+    }
+}
