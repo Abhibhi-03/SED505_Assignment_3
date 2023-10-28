7e626e98a3b74dfb63a055d555cef944bf5cf375
Abhi Patel <apatel477@myseneca.ca>
2023-10-27 23:03:05 -0400

    Added Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a facade for the multimedia system
+        MultimediaFacade multimediaFacade = new MultimediaFacade();
+
+        // Watch a movie using the facade
+        multimediaFacade.WatchMovie("Inception");
+
+        // End the movie using the facade
+        multimediaFacade.EndMovie();
+
+        Console.ReadKey();
+    }
+}
