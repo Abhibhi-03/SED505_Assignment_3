6b44605bc547cf6a4f11fde1a01a5c392d98790e
Abhi Patel <apatel477@myseneca.ca>
2023-10-27 23:04:10 -0400

    Added AudioSystem.cs

---
+++ AudioSystem.cs
@@ -0,0 +1,12 @@
+class AudioSystem
+{
+    public void TurnOn()
+    {
+        Console.WriteLine("Audio system is on.");
+    }
+
+    public void TurnOff()
+    {
+        Console.WriteLine("Audio system is off.");
+    }
+}
